fcc491dcb8950b76313c04e87c1c3098a79624a8
Francis Bello <bellofrancis87@gmail.com>
2022-11-13 22:27:10 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,25 @@ import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "../styles/globals.scss";
 import { motion } from "framer-motion";
+import * as ga from "../lib/ga";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps, router }) {
+  const routers = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    routers.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      routers.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [routers.events]);
   return (
     <motion.div
       key={router.route}
